85f06d71a2cc920516b01e6ca019c683d7451aef
mitchdmarino <mitchdmarino@gmail.com>
2022-07-19 14:20:36 -0700

    builds out private Routines page

--- flex/settings.py
+++ flex/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'workout',
     'base', 
+    'dashboard'
     
 
 ]
--- flex/urls.py
+++ flex/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('workout/', include('workout.urls') ),
     path('__debug__/', include('debug_toolbar.urls')),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path('dashboard/', include('dashboard.urls')),
 ]
