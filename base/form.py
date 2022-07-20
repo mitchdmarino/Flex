febb5c1209c79ac221e8e0be3576bfd0491f7a48
mitchdmarino <mitchdmarino@gmail.com>
2022-07-20 00:57:21 -0700

    adds styling to profile page and login/register forms. Initializes weight tracker app

--- base/form.py
+++ base/form.py
@@ -1,11 +1,35 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
 
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2' )
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class':'form-control form-control-md  ', 
+                'style': 'width: 300px;',
+                
+            }), 
+            
+            'email': forms.EmailInput(attrs={
+                'class':'form-control  ', 
+                'style': 'width: 300px;',
+                
+            }), 
+            'password2': forms.PasswordInput(attrs={
+                'class':'form-control ', 
+                'style': 'width: 400px',
+                
+            }), 
+            'password1': forms.PasswordInput(attrs={
+                'class':'form-control', 
+                'style': 'width: 400px',
+                
+            }), 
+        }
 
         def save(self, commit=True):
             user = super(SignUpForm, self).save(commit=False)
